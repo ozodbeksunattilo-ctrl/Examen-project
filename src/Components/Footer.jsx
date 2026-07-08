1e7c2c4364a9c80f08b3994f6b4be4276d05982c
ozodbeksunattilo <ozodbeksunattilo@gmail.com>
2026-07-09 01:25:13 +0500

    Header Footer add

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,8 +1,83 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { FaPaperPlane } from "react-icons/fa";
+import { FaFacebookF } from "react-icons/fa";
+import { AiFillInstagram } from "react-icons/ai";
+
+import { SiYoutube } from "react-icons/si";
+
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className='bg-[#01AEE7] '>
+<div className='max-w-[1300px] mx-auto flex'>
+<div className='py-[44px] px-[50px] flex-col items-center'>
+<div className='flex flex-col gap-[5px]'>
+    <h1 className='text-[white]'>КАТАЛОГ ТОВАРОВ</h1>
+  <NavLink className="text-[13px] text-[white] mt-[20px]">Кардио тренажеры</NavLink>
+  <NavLink className="text-[13px] text-[white] w-[150px]">Composite Strength</NavLink>
+  <NavLink className="text-[13px] text-[white]">Stretch</NavLink>
+  <NavLink className="text-[13px] text-[white]">Сайклинг</NavLink>
+  <NavLink className="text-[13px] text-[white]">Групповые тренировки</NavLink>
+  <NavLink className="text-[13px] text-[white]">Силовые тренажеры</NavLink>
+  <NavLink className="text-[13px] text-[white]">Консоли</NavLink>
+  <p className='py-[15px] text-[white]'>© TRUE FITNESS</p>
+
+</div>
+
+</div>
+<div className='py-[44px] px-[100px] flex-col items-center'>
+  <div className='flex flex-col gap-[5px]'>
+    <h1 className='text-[white]'>ИНФОРМАЦИЯ</h1>
+  <NavLink className="text-[13px] text-[white] mt-[20px]" to={"/"}>Home</NavLink>
+  <NavLink className="text-[13px] text-[white] " to="/diyor" >Преимущества</NavLink>
+  <NavLink className="text-[13px] text-[white]" to="/axror">открыть клуб</NavLink>
+  <NavLink className="text-[13px] text-[white]" to="/isroil">продукции</NavLink>
+  <NavLink className="text-[13px] text-[white]" to="/ramazon">контакты</NavLink>
+  
+
+</div>
+</div>
+
+<div>
+
+<div className='py-[44px] px-[100px] flex-col items-center'>
+  <div className='flex flex-col gap-[5px]'>
+    <h1 className='text-[white]'>КОНТАКТЫ</h1>
+  <NavLink className="text-[13px] text-[white] mt-[20px]">Политика конфиденциальности</NavLink>
+  <NavLink className="text-[13px] text-[white] ">Контакты</NavLink>
+ 
+  
+
+</div>
+</div>
+</div>
+<div className='py-[34px] px-[100px] flex-col items-center'>
+  <h1 className='text-[white]'>ПОДПИСАТЬСЯ НА НОВОСТИ И АКЦИИ</h1>
+  <div className='flex items-center gap-[8px] mt-[32px] '>
+    <input className='bg-[white] py-[10px] px-[10px]' placeholder='e - mail' type="email" />
+    <button className=' bg-[yellow] py-[15px] px-[20px] text-[20px] text-black'>
+    <FaPaperPlane />
+    </button>
+  </div>
+<div className='flex items-center gap-[10px] text-[white] text-[25px] py-[10px]'>
+  <NavLink className="text-[20px]" ><FaFacebookF /></NavLink>
+  <NavLink className="text-[25px]"><AiFillInstagram /></NavLink>
+  <NavLink className="text-[25px]"><SiYoutube /></NavLink>
+  
+  
+
+
+</div>
+
+</div>
+</div>
+
+
+
+
+    </div>
   )
 }
 
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
-
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
+import Logo from "../assets/Logo.png";
+import Flag from "../assets/flag.png";
 function Header() {
   return (
-    <div>Header</div>
-  )
+  
+  <div className="max-w-[1300px] mx-auto">
+
+ 
+  <div className="flex justify-between items-center py-[24px] border-b border-black mx-auto">
+    <img src={Logo} alt="" className="w-[350px] mx-auto mr-[300px]" />
+
+    <div className="flex flex-col items-end gap-4">
+      <div className="flex items-center gap-3">
+        <select>
+          <option>Rus</option>
+          <option>Uzb</option>
+        </select>
+
+        <span>+998 (90)-606-66-66</span>
+      </div>
+
+      <button type="submit" className="py-[10px] px-[20px] bg-[#01AEE7] text-white active:bg-blue-800 ">
+        оставить заявку
+      </button>
+    </div>
+  </div>
+
+
+  <div className="flex gap-[112px] py-[16px] items-center justify-center">
+    <NavLink className= "active:text-indigo-900 hover:mt-[-3px] hover:border-b-2  border-black hover:text-blue-900"  to="/">Home</NavLink>
+    <NavLink className= "active:text-indigo-900 hover:mt-[-3px] hover:border-b-2  border-black hover:text-blue-900" to="/diyor">о бренде</NavLink>
+    <NavLink className= "active:text-indigo-900 hover:mt-[-3px] hover:border-b-2  border-black hover:text-blue-900" to="/axror">преимущества</NavLink>
+    <NavLink className= "active:text-indigo-900 hover:mt-[-3px] hover:border-b-2  border-black hover:text-blue-900" to="/isroil">открыть клуб</NavLink>
+    <NavLink className= "active:text-indigo-900 hover:mt-[-3px] hover:border-b-2  border-black hover:text-blue-900" to="/ramazon">контакты</NavLink>
+  </div>
+
+</div>
+  );
 }
 
-export default Header
+export default Header;
